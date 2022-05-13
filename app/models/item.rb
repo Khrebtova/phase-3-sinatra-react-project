a0b3c4e75a37aad7b0627aef214635d0748a515e
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-05-13 14:36:50 -0700

    add one-to-many relationship in models

---
+++ app/models/item.rb
@@ -0,0 +1,19 @@
+class Item < ActiveRecord::Base
+    belongs_to :category
+
+
+    validates :name, presence: true
+    validates :category_id, presence: true
+    validates :completed, inclusion: { in: [true, false] }
+
+    def errors_array
+        self.errors.full_messages
+    end
+
+    def self.by_name
+        self.name.downcase
+    end
+
+end
+
+
